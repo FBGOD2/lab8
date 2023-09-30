99451eb67b9d26e8e18dffea47317ec378c81926
FBGOD2 <s.vxncxd.s@gmail.com>
2023-09-30 11:03:34 +0300

    added time.pas file that gets year type

---
+++ time.pas
@@ -0,0 +1,17 @@
+﻿begin
+  var year := ReadInteger('Введите год');
+  if year mod 4 = 0 then
+    if (year mod 100 = 0) and (year mod 400 <> 0) then
+        year := 365
+    else
+        year := 366
+   else
+      year := 365;
+ 
+PrintLn('Количество дней в году: ', year);
+
+if year = 365 then
+  println('Это невисокосный год.')
+else
+  writeln('Это високосный год.');
+end.
